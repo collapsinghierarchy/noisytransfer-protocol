edd5565749dd95e04ec6c271425c709181a4b871
collapsinghierarchy <58694234+collapsinghierarchy@users.noreply.github.com>
2025-08-24 13:49:02 +0200

    remove the old test directory and move the tests to their according package.

--- packages/crypto/src/index.js
+++ packages/crypto/src/index.js
@@ -1,6 +1,7 @@
 // Public crypto surface; primitives unchanged. Adapters live here too.
 
 export * from "./aead.js";
+export * from "./sas.js";
 export * from "./stream.js";
 export * from "./hash.js";
 export * from "./signature.js";
